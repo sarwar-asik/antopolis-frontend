3a87a61433429003c564aca222fca20bde908a60
sarwar-asik <sarwarasik@gmail.com>
2024-07-14 02:19:08 +0600

    build(app): build the app @build

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,1 +1,2 @@
-export const serverUrl = "http://localhost:5001/api/v1";
+export const serverUrl = "https://antp-server.vercel.app/api/v1";
+// export const serverUrl = "http://localhost:5001/api/v1";
